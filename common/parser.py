01d1587da043cd1b230d0316bf11e19176ccd8ab
Edmund Martin <edmartin101@googlemail.com>
2017-09-30 17:29:59 +0100

    Add files via upload

--- common/parser.py
+++ common/parser.py
@@ -1,10 +1,12 @@
 import lxml.html as lh
-from urllib.parse import urljoin
+from urllib.parse import urljoin, urlparse
 
 
 def get_all_links(crawled_url, original_url, html):
     found_urls = []
     dom = lh.fromstring(html)
+    parsed = urlparse(original_url)
+    original_url = '{}://{}'.format(parsed.scheme, parsed.netloc)
     for href in dom.xpath('//a/@href'):
         url = urljoin(crawled_url, href)
         if url.startswith(original_url):
